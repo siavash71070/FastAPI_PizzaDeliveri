5f7790d0c68b025bc20db04f5c3f5b173198216a
siavash <bidadparhizkar@yahoo.com>
2025-01-10 17:01:57 +0330

    added order routes sample.

---
+++ order_routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+order_router = APIRouter(
+    prefix="/order",
+    tags=["orders"]
+)
+
+
+@order_router.get("/")
+async def hello():
+    return {"message": "This Is Orders"}
