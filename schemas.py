a5ed108feb2e9be951062eb5d5e5b4677c11c3c9
siavash <bidadparhizkar@yahoo.com>
2025-01-18 00:40:28 +0330

    add order status model for update orders.

--- schemas.py
+++ schemas.py
@@ -46,3 +46,17 @@ class OrderModel(BaseModel):
                 "pizza_size": "LARGE",
             }
         }
+
+
+
+class OrderStatusModel(BaseModel):
+
+    order_status: Optional[str] = "PENDING"
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "order_status": "PENDING"
+            }
+        }
